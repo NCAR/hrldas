a977cc31cbfb93e389d3b4c12532cce0046495f4
Zhe Zhang <zhe.zhang@usask.ca>
2023-11-27 22:27:22 -0700

    Code to prepare ERA5-Land forcing in develop branch

--- hrldas/HRLDAS_forcing/run/examples/ERA5/extract_era5_init.py
+++ hrldas/HRLDAS_forcing/run/examples/ERA5/extract_era5_init.py
@@ -10,24 +10,27 @@ import numpy as np
 import sys,os
 
 ### create date arrays
-yr   = "01"
-date = "1001"
-cc   = "20"   # Manually set the century
+year  = "2023"
+month = "11"
+day   = "01"
 
 vars_name = ["SKT","SD","STL1","STL2","STL3","STL4","SWVL1","SWVL2","SWVL3","SWVL4"]
 vars_time = ["hr fcst:","hr fcst:","hr fcst:","hr fcst:","hr fcst:","hr acc:","hr acc:","hr acc:"]
 
-data_dir = "/glade/scratch/zhezhang/ERA5/land/"
-infile  = data_dir+"ERA5_global_2001-10-01_init.grib"
-results_dir = "/glade/scratch/zhezhang/ERA5/land/extract/INIT/"
+data_dir = "../../../ERA5_forcing/"
+infile  = data_dir+"ERA5-Land-Noah-MP-2023_11_01_INIT.grib"
+results_dir = "./OUTPUT/INIT/"
+
+os.system("mkdir -p "+results_dir)
 
 for var in range(10):
-    print "working on time: ",cc+yr+date+"00"
-    dcommand = "d="+cc+yr+date+"00:"+vars_name[var]
-    outfile= results_dir+"/ERA5_"+vars_name[var]+"_"+cc+yr+date+"00"
+    print("working on time: ",year+month+day+"00")
+    dcommand = "d="+year+month+day+"00:"+vars_name[var]
+    outfile= results_dir+"/ERA5_"+vars_name[var]+"_"+year+month+day+"00"
     if not os.path.exists(outfile):
         os.system("wgrib -s -4yr "+infile+" | grep '"+dcommand+"' | wgrib -i -grib "+infile+" -o "+outfile)
     else:
-        print "file exist, move to next one"
+        print("file exist, move to next one")
 
-print "Successfully extract necessary variables!"
+print("Successfully extract necessary variables!")
+
